573e89614882283f982648dac928d375155cf5f4
Khalid Afridi <afridikhalid@yahoo.com>
2024-05-19 20:02:17 +0200

    Initial Commit

---
+++ Awesome Movies/Awesome_MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Awesome_MoviesApp.swift
+//  Awesome Movies
+//
+//  Created by Khalid Afridi on 2024-05-19.
+//
+
+import SwiftUI
+
+@main
+struct Awesome_MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Awesome Movies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Awesome Movies
+//
+//  Created by Khalid Afridi on 2024-05-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
